166bac161ebf78dfdd9e16f1b29d002b42d83bd6
Luke Evers <luke@lukevers.com>
2017-02-15 18:51:13 -0500

    Include patch in version.
    macpack doesn't like it, but it still builds it fine.

--- app/version.go
+++ app/version.go
@@ -3,7 +3,7 @@ package main
 var (
 	// Version is the version number that this package was built on. This
 	// variable should be set by the linker.
-	Version = "0.0"
+	Version = "0.0.0"
 
 	// Commit is the commit that this package was built on. This variable should
 	// be set by the linker.
