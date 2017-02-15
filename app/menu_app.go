fd02efca2b72bc77fa4a5dcea000c816906657e6
Luke Evers <luke@lukevers.com>
2017-02-15 18:58:57 -0500

    Add comments on structs and functions for menus

--- app/menu_app.go
+++ app/menu_app.go
@@ -5,12 +5,14 @@ import (
 	"github.com/murlokswarm/app"
 )
 
+// MenuApp is a menu at the application level.
 type MenuApp struct{}
 
 func init() {
 	app.RegisterComponent(&MenuApp{})
 }
 
+// Render compiles the template for the MenuApp
 func (m *MenuApp) Render() string {
 	var content bytes.Buffer
 	err := Templates.ExecuteTemplate(&content, "menu-app", m)
@@ -21,6 +23,8 @@ func (m *MenuApp) Render() string {
 	return content.String()
 }
 
+// OpenAboutWindow opens a new PageAbout in a new window if a window for it
+// does not already exist.
 func (m *MenuApp) OpenAboutWindow() {
 	if aboutWindow == nil {
 		aboutWindow = newAboutWindow()
--- app/menu_window.go
+++ app/menu_window.go
@@ -5,12 +5,14 @@ import (
 	"github.com/murlokswarm/app"
 )
 
+// MenuWindow is a menu at the window level.
 type MenuWindow struct{}
 
 func init() {
 	app.RegisterComponent(&MenuWindow{})
 }
 
+// Render compiles the template for the MenuWindow
 func (m *MenuWindow) Render() string {
 	var content bytes.Buffer
 	err := Templates.ExecuteTemplate(&content, "menu-window", m)
